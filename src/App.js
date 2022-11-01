7daa96e1704986d20ec8d68ef9b4cf8ac5e70764
Ahumada-Alejandro <camiloalejandroahumada@gmail.com>
2022-10-31 22:07:52 -0500

    hola colombia

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hola mundo
+          Hola colombia
         </a>
       </header>
     </div>
